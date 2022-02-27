b8e58fe87060e1cd673f9af6395195b6fc2e4132
huynhimo635 <huynh.nguyen.imo@gmail.com>
2022-02-27 14:25:25 +0700

    setup structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 module.exports = {
-  mode: 'jit',
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    mode: 'jit',
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    important: true,
+    // Active dark mode on class basis
+    darkMode: "class",
+    i18n: {
+        locales: ["en-US"],
+        defaultLocale: "en-US",
+    },
+    theme: {
+        extend: {
+            // eslint-disable-next-line no-unused-vars
+            backgroundImage: (theme) => ({
+                check: "url('/icons/check.svg')",
+                landscape: "url('/images/landscape/2.jpg')",
+            }),
+        },
+    },
+    variants: {
+        extend: {
+            backgroundColor: ["checked"],
+            borderColor: ["checked"],
+            inset: ["checked"],
+            zIndex: ["hover", "active"],
+        },
+    },
+    plugins: [],
+    future: {
+        purgeLayersByDefault: true,
+    },
 };
